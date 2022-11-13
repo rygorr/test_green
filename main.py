2032e9b0c59fadf495b3f5566a6cf8a67e680c9d
Игорь Пынзарь <pinzarigr@gmail.com>
2022-11-13 23:48:58 +0300

    commit

--- main.py
+++ main.py
@@ -8,11 +8,19 @@ s = Service('C:/webdr/chromedriver.exe')
 driver = webdriver.Chrome(service=s, options=options)
 url = 'https://www.moex.com/'
 driver.get(url)
-time.sleep(1)
+time.sleep(2)
 driver.find_element(By.CLASS_NAME, "header-nav__mobile-menu-button").click()
 time.sleep(1)
 derivative = driver.find_element(By.LINK_TEXT, 'Срочный рынок')
 derivative.click()
-indicative = driver.find_element(By.XPATH, '//*[@id="ctl00_frmLeftMenuWrap"]/div/div/div[3]/div[13]/div/a')
-indicative.click()
+# принятие соглашения
+try:
+    driver.find_element(By.XPATH, '//*[@id="content_disclaimer"]/div/div/div/div[1]/div/a[1]').click()
+except Exception:
+    print('Соглашение уже принято')
+time.sleep(1)
+driver.find_element(By.XPATH, '//*[@id="ctl00_frmLeftMenuWrap"]/div/div/div[2]').click()
+time.sleep(1)
+driver.find_element(By.XPATH, '//*[@id="ctl00_frmLeftMenuWrap"]/div/div/div[3]/div[13]/div').click()
 time.sleep(5)
+#дошел до нужной страницыы
